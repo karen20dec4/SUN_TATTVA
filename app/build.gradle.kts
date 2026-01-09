040b3e431b640c47baf49064eff82b0f6bc275de
karen20dec4 <karen20dec4@yahoo.com>
2026-01-10 00:53:07 +0200

    Modificări de Magurele - version 2.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "2.1"
+        versionName = "2.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
